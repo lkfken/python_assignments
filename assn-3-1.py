e06b0710af962c0ba61f194549b3936074212743
Kenneth Leung <kenneth@leungs.us>
2015-06-26 11:58:21 -0700

    This assignment shows  1. how to open a file and read the content.  2. how to check the beginning of a string  3. how to print a floating number with predefine precision

--- assn-3-1.py
+++ assn-3-1.py
@@ -1,14 +1,12 @@
 __author__ = 'kleung'
 
-"""
-3.1 Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay.
-1) Pay the hourly rate for the hours up to 40 and
-2) 1.5 times the hourly rate for all hours worked above 40 hours.
-
-Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
-You should use raw_input to read a string and float() to convert the string to a number.
-Do not worry about error checking the user input - assume the user types numbers properly.
-"""
+# 3.1 Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay.
+# 1) Pay the hourly rate for the hours up to 40 and
+# 2) 1.5 times the hourly rate for all hours worked above 40 hours.
+#
+# Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
+# You should use raw_input to read a string and float() to convert the string to a number.
+# Do not worry about error checking the user input - assume the user types numbers properly.
 
 OVERTIME_RATE = 1.5
 MAX_REGULAR_HOURS = 40
