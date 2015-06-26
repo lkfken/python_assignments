869a2291c20e7d9187d6a7f935a767a0fe05b6f9
Kenneth Leung <kenneth@leungs.us>
2015-06-26 11:57:23 -0700

    This assignment shows  1. how to open a file and read the content.  2. how to check the beginning of a string  3. how to print a floating number with predefine precision

---
+++ assn-7-2.py
@@ -0,0 +1,30 @@
+__author__ = 'kleung'
+
+# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file,
+# looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+#
+# Count these lines and extract the floating point values from each of the lines and compute the average of those
+# values and produce an output as shown below.
+#
+# You can download the sample data at http://www.pythonlearn.com/code/mbox-short.txt when you are testing below
+# enter mbox-short.txt as the file name.
+
+# Use the file name mbox-short.txt as the file name
+
+fname = raw_input("Enter file name: ")
+# fname = 'assn-7-1/mbox-short.txt'
+fh = open(fname)
+count = 0
+sum = 0.0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    count += 1
+    pos = line.find(':')
+    str_num = line[pos + 1:]
+    num = float(str_num)
+    sum += num
+print "Average spam confidence: {0:.12f}".format(sum/count)
+
+
